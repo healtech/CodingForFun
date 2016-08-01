9f2a72e62750ed9f971c6274c0c5f150d31d260c
Shelvin Yuan <fairwind83@hotmail.com>
2016-08-01 09:36:53 +0800

    Create leetcode378.js

---
+++ leetcode378.js
@@ -0,0 +1,143 @@
+function CMP(l,r) { return l-r; }
+
+function MinHeap(scoreFn) {
+  this.cmp = scoreFn || CMP;
+  this.heap = [];
+  this.size = 0;
+}
+
+MinHeap.prototype = {
+  
+  insert: function(item) {
+    
+    var heap  = this.heap,
+        ix    = this.size++;
+        
+    heap[ix] = item;
+    
+    var parent = (ix-1)>>1;
+    
+    while ((ix > 0) && this.cmp(heap[parent], item) > 0) {
+      var tmp = heap[parent];
+      heap[parent] = heap[ix];
+      heap[ix] = tmp;
+      ix = parent;
+      parent = (ix-1)>>1;
+    }
+        
+  },
+  
+  removeHead: function() {
+    
+    var heap  = this.heap,
+        cmp   = this.cmp;
+    
+    if (this.size === 0)
+      return undefined;
+      
+    var out = heap[0];
+    
+    this._bubble(0);
+    
+    return out;
+    
+  },
+
+  remove: function(item) {
+
+    var heap = this.heap;
+
+    for (var i = 0; i < this.size; ++i) {
+      if (heap[i] === item) {
+        this._bubble(i);
+        return true;
+      }
+    }
+
+    return false;
+
+  },
+
+  _bubble: function(ix) {
+
+    var heap  = this.heap,
+        cmp   = this.cmp;
+
+    heap[ix] = heap[--this.size];
+    heap[this.size] = null;
+
+    while (true) {
+      
+      var leftIx  = (ix<<1)+1,
+          rightIx = (ix<<1)+2,
+          minIx   = ix;
+      
+      if (leftIx < this.size && cmp(heap[leftIx], heap[minIx]) < 0) {
+        minIx = leftIx;
+      }
+      
+      if (rightIx < this.size && cmp(heap[rightIx], heap[minIx]) < 0) {
+        minIx = rightIx;
+      }
+      
+      if (minIx !== ix) {
+        var tmp = heap[ix];
+        heap[ix] = heap[minIx];
+        heap[minIx] = tmp;
+        ix = minIx;
+      } else {
+        break;
+      }
+      
+    }
+
+  }
+
+};
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    
+    var heap = new MinHeap(function(node1, node2) {
+        return matrix[node1.x][node1.y] - matrix[node2.x][node2.y] ;
+    });
+    var cache =  new Set();
+    var node = {x:0, y: 0};
+    heap.insert(node);
+    cache.add('0+0');
+    var i = 0;
+    var kSt;
+    
+    while(i < k) {
+        var head = heap.removeHead();
+        
+        if(head === undefined) {
+            //heap is empty
+            break;
+        }
+        ksp = matrix[head.x][head.y];
+        
+        if(head.y + 1 < matrix.length) {
+            var right = {x: head.x, y: head.y + 1};
+            if(!cache.has(right.x+ '+' + right.y)) {
+                heap.insert(right);
+                cache.add(right.x+ '+' + right.y);
+            }
+        }
+        if(head.x + 1 < matrix.length) {
+            var bottom = {x: head.x +1, y : head.y};
+            
+            if(!cache.has(bottom.x+ '+' + bottom.y)) {
+                heap.insert(bottom);
+                cache.add(bottom.x+ '+' + bottom.y)
+            } 
+        }
+        i++;
+    }
+    
+    return ksp;
+};
